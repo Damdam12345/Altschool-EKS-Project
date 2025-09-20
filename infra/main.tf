a55af9495671da21e8d35cb6675ff2ed3713370e
damilare <onifadeayomide11@gmail.com>
2025-09-20 12:01:08 +0100

    Fixed main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -12,9 +12,25 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
   }
 }
 
 provider "aws" {
   region = "eu-west-1"
+}
+# Kubernetes provider configuration
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name, "--region", var.aws_region]
+  }
 }
